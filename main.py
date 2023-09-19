38f7cbf22749c6afbe5517384e129fd36943ea94
Bruna Cordeiro <bruna_paula2002@hotmail.com>
2023-09-19 16:08:00 -0300

    initial screen

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame, sys
+from settings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.tela = pygame.display.set_mode((LARGURA_TELA, ALTURA_TELA))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            dt = self.clock.tick() / 1000
+            pygame.display.update()
+
+if __name__ == '__main__':
+    jogo = Game()
+    jogo.run()
+
---
+++ settings.py
@@ -0,0 +1,8 @@
+from pygame.math import Vector2
+
+#tela
+LARGURA_TELA = 1280
+ALTURA_TELA = 720
+TILE_SIZE = 64
+
+#POSICOES 
