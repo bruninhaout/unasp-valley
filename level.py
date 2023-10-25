926af75f87d7b06001afd12d126b264e3326b86a
EA - Nicolas Ferreira Perejon Amorim <nicolas_amorim@educadventista.org>
2023-10-25 20:29:56 -0300

    feat: map

--- level.py
+++ level.py
@@ -20,7 +20,8 @@ class Level:
         Generic(
             pos = (0,0),
             surf = pygame.image.load('./graficos/world/ground.png').convert_alpha(),
-            groups = self.all_sprites
+            groups = self.all_sprites,
+            z = LAYERS['ground']
         )
 
     def run(self, dt): #superficie que irá sempre atualizar
@@ -32,7 +33,14 @@ class CameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2()
 
-    def custom_draw(self):
-        for sprite in self.sprites():
-            self.display_surface.blit(sprite.image, sprite.rect)
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - SCREEN_WIDTH / 2
+        self.offset.y = player.rect.centery - SCREEN_HEIGHT  / 2
+        for layer in LAYERS.values():
+            for sprite in self.sprites():
+                if sprite.z == layer:
+                    offset_rect = sprite.rect.copy()
+                    offset_rect.center -= self.offset
+                    self.display_surface.blit(sprite.image, offset_rect)
