fcf10e7cf8f34a0635e623e62dd972e388f7391f
EA - Nicolas Ferreira Perejon Amorim <nicolas_amorim@educadventista.org>
2023-10-25 20:08:38 -0300

    feat: map

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from player import Player
+from sprites import Generic
 
 class Level:
 
@@ -10,14 +11,28 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         #sprite groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = CameraGroup()
 
         self.setup()
 
     def setup(self):
+        Generic(
+            pos = (0,0),
+            surf = pygame.image.load('./graficos/world/ground.png').convert_alpha(),
+            groups = self.all_sprites
+        )
         self.player = Player((640, 360), self.all_sprites) #posição e grupo do player
 
     def run(self, dt): #superficie que irá sempre atualizar
         self.display_surface.fill('black')
-        self.all_sprites.draw(self.display_surface) #função draw dos sprites groups
+        self.all_sprites.custom_draw()
         self.all_sprites.update(dt) #update method
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+    def custom_draw(self):
+        for sprite in self.sprites():
+            self.display_surface.blit(sprite.image, sprite.rect)
