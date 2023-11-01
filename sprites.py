aa0472e5a2370407c3891262e1069022459bad3e
EA - Vinicius Custodio Abe Da Silva <custodio_abe@educadventista.org>
2023-11-01 19:58:54 -0300

    Add Particles func

--- sprites.py
+++ sprites.py
@@ -36,6 +36,23 @@ class WildFlower(Generic):
     def __init__(self, pos, surf, groups):
         super().__init__(pos, surf, groups)
 
+class Particle(Generic):
+	def __init__(self, pos, surf, groups, z, duration = 200):
+		super().__init__(self, surf, groups, z)
+		self.start_time = pygame.time.get_ticks()
+		self.duration = duration
+
+		# superfície branca
+		mask_surf = pygame.mask.from_surface(self.image)
+		new_surf = mask_surf.to_surface()
+		new_surf.set_colorkey(0,0,0)
+		self.image = new_surf
+
+		def update(self, dt):
+			current_time = pygame.time.get_ticks()
+			if current_time - self.start_time > self.duration:
+				self.kill()
+
 class Tree(Generic):
     def __init__(self, pos, surf, groups, name):
         super().__init__(pos, surf, groups)
